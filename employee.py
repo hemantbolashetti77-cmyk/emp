b4c0aa7237142b09617e666812f316069a774587
hemant17 <hemantbolashetti77@gmail.com>
2025-12-11 15:14:21 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     name = "Alice"
     emp_id = "E1001"
     department = "IT"
-    salary = 50000
+    salary = 55000
 
     print(employee_details(name, emp_id, department, salary))
